883335689ad240739049009e4fc6af539f90f563
Marcin Gizka <czarnymax112@gmail.com>
2023-04-12 11:33:23 +0200

    add folders with graphics images and photos mages, create gallery with photos and graphics

--- my-app/src/components/Gallery/GraphGalery.jsx
+++ my-app/src/components/Gallery/GraphGalery.jsx
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Carousel from "react-bootstrap/Carousel";
+
+import "./Gallery.scss";
+import chess from "../../images/GraphGalleryImages/chess.jpg";
+import fish from "../../images/GraphGalleryImages/fish.jpg";
+import fotograf from "../../images/GraphGalleryImages/fotograf.jpg";
+import kurczak from "../../images/GraphGalleryImages/kurczak.jpg";
+import moon from "../../images/GraphGalleryImages/moon.jpg";
+import myszkaMinnie from "../../images/GraphGalleryImages/myszkaMinnie.jpg";
+import piorko from "../../images/GraphGalleryImages/piorko.jpg";
+import rak from "../../images/GraphGalleryImages/rak.jpg";
 
 const GraphGallery = () => {
-  return <h1>GraphGallery</h1>;
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <section className="galleryWrapper">
+      <Carousel
+        variant="dark"
+        className="gallery__carousel"
+        activeIndex={index}
+        onSelect={handleSelect}
+      >
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={chess}
+            alt="figura szachowa wpadająca do wody"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={fish}
+            alt="dwie ryby zamknięte w żarówce z wodą"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={fotograf}
+            alt="fotograf robiący zdjecie"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={kurczak}
+            alt="kurczak ubrany w strój zawodnika taekwondoo"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={moon}
+            alt="księżyć zakryty gałeziami drzew"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={myszkaMinnie}
+            alt="grafika przedstawiająca myszkę Minnie"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={piorko}
+            alt="kolorowe piórko"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={rak}
+            alt="grafika przedstawiająca znak zodiaku- rak"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </section>
+  );
 };
 
 export default GraphGallery;
--- my-app/src/components/Gallery/PhotoGallery.jsx
+++ my-app/src/components/Gallery/PhotoGallery.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Carousel from "react-bootstrap/Carousel";
+
+import "./Gallery.scss";
+import blacksmith from "../../images/PhotoGalleryImages/blacksmith.jpg";
+import blueflower from "../../images/PhotoGalleryImages/blueflower.jpg";
+import fly from "../../images/PhotoGalleryImages/fly.jpg";
+import frog from "../../images/PhotoGalleryImages/frog.jpg";
+import girl from "../../images/PhotoGalleryImages/girl.jpg";
+import redflower from "../../images/PhotoGalleryImages/redflower.jpg";
 
 const PhotoGallery = () => {
-  return <h1>PhotoGallery</h1>;
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <section className="galleryWrapper">
+      <Carousel
+        className="gallery__carousel"
+        activeIndex={index}
+        onSelect={handleSelect}
+        variant="dark"
+      >
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={blacksmith}
+            alt="rozprysk iskier podczas kucia żelaza "
+          />
+        </Carousel.Item>
+
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={blueflower}
+            alt="zdjęcie niebieskiego kwiatu"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={fly}
+            alt="zdjęcie owada siedzącego na kwiecie"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={frog}
+            alt="zdjęcie żaby siedzącej na jesiennych liściach"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={girl}
+            alt="portretowe zdjęcie małej dziewczynki na tle drzew"
+          />
+        </Carousel.Item>
+
+        <Carousel.Item className="gallery__carousel-item">
+          <img
+            className="d-block w-100 img-fluid gallery__carousel-item-image"
+            src={redflower}
+            alt="zdjęcie czerwonego kwiatu"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </section>
+  );
 };
 
 export default PhotoGallery;
